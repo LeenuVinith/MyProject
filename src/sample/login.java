0e251a549da929d54d21859f13d44753fed873bb
Leenu <leenu@calibraint.com>
2022-04-18 16:27:12 +0530

    First commit..

---
+++ src/sample/login.java
@@ -0,0 +1,14 @@
+package sample;
+
+
+public class login {
+
+	WebDriver driver= new Firefoxdriver();
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
